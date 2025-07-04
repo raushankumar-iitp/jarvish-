de00821003dc2d0051755e218d33bd29a842af70
raushankumar-iitp <raushan_24a12res1049@iitp.ac.in>
2025-07-04 14:59:45 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "mahuasingh": "https://www.youtube.com/watch?v=2CnatpIv8qQ&pp=ygUMa2FtYXIgY2hpa25p"
+}
